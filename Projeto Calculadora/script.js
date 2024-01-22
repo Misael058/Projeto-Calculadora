1358544a6962692e10c394bc914c2400ffc803f3
Misael sapiente <147666566+Misael058@users.noreply.github.com>
2024-01-22 13:57:42 -0300

    Add files via upload

---
+++ Projeto Calculadora/script.js
@@ -0,0 +1,8 @@
+document.getElementById("calculate").addEventListener("click", function() {
+    const valor = document.getElementById("value").value;
+    const taxa = document.getElementById("fee").value / 100;
+    const tempo = document.getElementById("time").value;
+
+    const total = valor * (1 + taxa ) ** tempo;
+    document.getElementById("total").innerHTML = ("R$" + total.toFixed(2)); 
+})
